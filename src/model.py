144acefb410a2a82289c6b6417d22df815e97957
guyj <guyj@sentinelone.com>
2022-02-22 20:27:49 +0200

    working on model

--- src/model.py
+++ src/model.py
@@ -5,7 +5,16 @@ import torch.nn as nn
 
 
 class Transformer(nn.Module):
-    def __init__(self, model_dimension, src_vocab_size, tgt_vocab_size, num_of_layers, num_of_attn_heads, ff_dimension, dropout):
+    def __init__(
+        self,
+        model_dimension,
+        src_vocab_size,
+        tgt_vocab_size,
+        num_of_layers,
+        num_of_attn_heads,
+        ff_dimension,
+        dropout,
+    ):
         super().__init__()
 
         self.src_embedding = Embedding(src_vocab_size, model_dimension)
@@ -24,7 +33,9 @@ class Transformer(nn.Module):
 
         self.output_generator = OutputGenerator()
 
-    def forward(self, ):
+    def forward(
+        self,
+    ):
         return
 
 
@@ -34,7 +45,6 @@ class Encoder(nn.Module):
         self.encoder_stack = get_copies(encoder_layer, num_of_layers)
 
     def forward(self, src_encoding, src_mask):
-
         for encoder_layer in self.encoder_stack:
             src_encoding = encoder_layer(src_encoding)
 
@@ -45,7 +55,8 @@ class EncoderLayer(nn.Module):
     def __init__(self, model_dimension, mha, ffn, dropout):
         super().__init__()
         self.mha_sublayer = Sublayer(module=mha)
-        self.feed_forward_sublayer = Sublayer(module=)
+        self.feed_forward_sublayer = Sublayer(module=ffn)
+
     def forward(self):
         return
 
@@ -55,7 +66,9 @@ class Decoder(nn.Module):
         super().__init__()
         self.decoder_stack = get_copies(decoder_layer, num_of_layers)
 
-    def forward(self, ):
+    def forward(
+        self,
+    ):
         return
 
 
@@ -110,7 +123,13 @@ class Embedding(nn.Module):
         self.scaling_factor = math.sqrt(model_dimension)
 
     def forward(self, token_ids):
+        assert (
+            token_ids.ndim == 2
+        ), f"Expected 2 dimensions for batch token ids (batch size, max sequence length), got {token_ids.shape}"
+
         embeddings = self.embedding(token_ids)
+
+        # As in the paper, the learned embeddings are scaled by a factor of sqrt(model_dimension)
         return embeddings * self.scaling_factor
 
 
@@ -119,11 +138,24 @@ class PositionalEncoding(nn.Module):
         super().__init__()
         self.dropout = nn.Dropout(dropout)
 
-    def frequency_calc(self):
-        pass
+        self.pos = torch.arange(max_sequence_size).reshape(-1, 1)
+        self.div_freq = torch.exp(
+            torch.arange(0, model_dimension, 2)
+            / model_dimension
+            * math.log(1e4)
+        )
 
-    def forward(self, ):
-        return
+        # initialize positional encoding (pe) tensor
+        self.pe = torch.zeros((max_sequence_size, model_dimension))
+        self.pe[:, 0::2] = torch.sin(self.pos / self.div_freq)
+        self.pe[:, 1::2] = torch.cos(self.pos / self.div_freq)
+
+    def forward(self, embeddings):
+        assert self.pe.shape == embeddings.shape[1:], (
+            f"Mismatch between positional encoding tensor shape {self.pe.shape} and "
+            f"embeddings shape (without batch dim) {embeddings.shape[1:].shape} "
+        )
+        return embeddings + self.pe
 
 
 def get_copies(module, num_of_copies):
