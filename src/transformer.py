1843d0c43ecbd9c2ccb34ddad6c04fc4a79b4e13
guyj <guyj@sentinelone.com>
2022-02-24 17:02:43 +0200

    working on model

--- src/transformer.py
+++ src/transformer.py
@@ -5,35 +5,29 @@ import torch.nn as nn
 
 
 class Transformer(nn.Module):
-    def __init__(
-        self,
-        model_dimension = 512,
-        num_of_layers = 6,
-        num_of_attn_heads = 8,
-        ff_dimension = 2048,
-        dropout = 0.1,
-        activation = 'relu'
-    ):
+    def __init__(self, model_dimension=512, num_of_layers=6, num_of_attn_heads=8, ff_dimension=2048, dropout=0.1):
         super().__init__()
 
-
-        encoder_layer = EncoderLayer(model_dimension, num_of_attn_heads, ff_dimension, dropout, activation)
-        decoder_layer = DecoderLayer(model_dimension, dropout)
+        encoder_layer = EncoderLayer(model_dimension, num_of_attn_heads, ff_dimension, dropout)
+        decoder_layer = DecoderLayer(model_dimension, num_of_attn_heads, ff_dimension, dropout)
         self.encoder = Encoder(encoder_layer, num_of_layers)
         self.decoder = Decoder(decoder_layer, num_of_layers)
 
         self.output_generator = OutputGenerator()
 
-    def forward(
-        self,
-    ):
+    def forward(self, ):
         return
+
+
+################################
+### Core transformer modules ###
+################################
 
 
 class Encoder(nn.Module):
     def __init__(self, encoder_layer, num_of_layers):
-       super().__init__()
-       self.encoder_stack = get_copies(encoder_layer, num_of_layers)
+        super().__init__()
+        self.encoder_stack = get_copies(encoder_layer, num_of_layers)
 
     def forward(self, src_encoding, src_mask):
         for encoder_layer in self.encoder_stack:
@@ -43,7 +37,7 @@ class Encoder(nn.Module):
 
 
 class EncoderLayer(nn.Module):
-    def __init__(self, model_dimension, mha, ffn, dropout, activation='relu'):
+    def __init__(self, model_dimension, mha, ffn, dropout):
         super().__init__()
         self.mha_sublayer = Sublayer(module=mha)
         self.feed_forward_sublayer = Sublayer(module=ffn)
@@ -57,9 +51,7 @@ class Decoder(nn.Module):
         super().__init__()
         self.decoder_stack = get_copies(decoder_layer, num_of_layers)
 
-    def forward(
-        self,
-    ):
+    def forward(self, ):
         return
 
 
@@ -71,13 +63,21 @@ class DecoderLayer(nn.Module):
         return
 
 
+#####################################
+### Auxiliary transformer modules ###
+#####################################
+
+
 class Sublayer(nn.Module):
-    def __init__(self, module):
+    def __init__(self, module, model_dimension):
         super().__init__()
         self.module = module
+        self.norm = nn.LayerNorm(model_dimension)
 
-    def forward(self):
-        return
+    def forward(self, input):
+        output = input + nn.module(input)
+        output = self.norm(output)
+        return output
 
 
 class MultiHeadAttention(nn.Module):
@@ -91,10 +91,15 @@ class MultiHeadAttention(nn.Module):
 class FeedForwardNet(nn.Module):
     def __init__(self, model_dimension, ff_dimension):
         super().__init__()
-        self.linear1 = nn.Linear()
+        self.ff = nn.Sequential(
+            nn.Linear(model_dimension, ff_dimension),
+            nn.ReLU(),
+            nn.Linear(ff_dimension, model_dimension)
+        )
 
-    def forward(self):
-        return
+    def forward(self, input):
+        output = self.ff(input)
+        return output
 
 
 class OutputGenerator(nn.Module):
