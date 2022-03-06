000c181bee907c1035ce65ebb0dd0a8333b35049
Guy Jacoby <guyjacobyx@gmail.com>
2022-03-06 16:08:49 +0200

    working on model

--- src/model.py
+++ src/model.py
@@ -5,26 +5,29 @@ from transformer import Transformer
 
 
 class Model(nn.Module):
-    def __init__(self, model_dimension=512, num_of_layers=6, num_of_attn_heads=8, ff_dimension=2048, dropout=0.1):
+    def __init__(self, src_vocab_size, tgt_vocab_size, model_dim=512, num_of_layers=6, num_of_attn_heads=8,
+                 ffn_dim=2048, dropout=0.1):
         super().__init__()
 
-        self.src_embedding = Embedding(src_vocab_size, model_dimension)
-        self.tgt_embedding = Embedding(tgt_vocab_size, model_dimension)
+        self.src_embedding = Embedding(src_vocab_size, model_dim)
+        self.tgt_embedding = Embedding(tgt_vocab_size, model_dim)
 
-        self.src_positional_embedding = PositionalEncoding(model_dimension, dropout)
-        self.tgt_positional_embedding = PositionalEncoding(model_dimension, dropout)
+        self.src_positional_embedding = PositionalEncoding(model_dim, dropout)
+        self.tgt_positional_embedding = PositionalEncoding(model_dim, dropout)
 
-        self.transformer = Transformer(model_dimension, num_of_layers, num_of_attn_heads, ff_dimension, dropout)
+        self.transformer = Transformer(model_dim, num_of_layers, num_of_attn_heads, ffn_dim, dropout)
+        
+        self.output_generator = OutputGenerator(model_dim, tgt_vocab_size)
 
     def forward(self, src, src_mask, tgt, tgt_mask):
         return
 
 
 class Embedding(nn.Module):
-    def __init__(self, vocab_size, model_dimension):
+    def __init__(self, vocab_size, model_dim):
         super().__init__()
-        self.embedding = nn.Embedding(vocab_size, model_dimension)
-        self.scaling_factor = math.sqrt(model_dimension)
+        self.embedding = nn.Embedding(vocab_size, model_dim)
+        self.scaling_factor = math.sqrt(model_dim)
 
     def forward(self, token_ids):
         assert token_ids.ndim == 2, (
@@ -33,20 +36,20 @@ class Embedding(nn.Module):
 
         embeddings = self.embedding(token_ids)
 
-        # As in the paper, the learned embeddings are scaled by a factor of sqrt(model_dimension)
+        # As in the paper, the learned embeddings are scaled by a factor of sqrt(model_dim)
         return embeddings * self.scaling_factor
 
 
 class PositionalEncoding(nn.Module):
-    def __init__(self, model_dimension, dropout, max_sequence_size=10_000):
+    def __init__(self, model_dim, dropout, max_sequence_size=10_000):
         super().__init__()
         self.dropout = nn.Dropout(dropout)
 
         self.pos = torch.arange(max_sequence_size).reshape(-1, 1)
-        self.div_freq = torch.exp(torch.arange(0, model_dimension, 2) / model_dimension * math.log(1e4))
+        self.div_freq = torch.exp(torch.arange(0, model_dim, 2) / model_dim * math.log(1e4))
 
         # initialize positional encoding (pe) tensor
-        self.pe = torch.zeros((max_sequence_size, model_dimension))
+        self.pe = torch.zeros((max_sequence_size, model_dim))
         self.pe[:, 0::2] = torch.sin(self.pos / self.div_freq)
         self.pe[:, 1::2] = torch.cos(self.pos / self.div_freq)
 
@@ -57,3 +60,13 @@ class PositionalEncoding(nn.Module):
         )
 
         return self.dropout(embeddings + self.pe)
+
+
+class OutputGenerator(nn.Module):
+    def __init__(self, model_dim, tgt_vocab_size):
+        super().__init__()
+        self.linear = nn.Linear(model_dim, tgt_vocab_size)
+        self.softmax = nn.LogSoftmax(dim=-1)
+
+    def forward(self):
+        return
--- src/transformer.py
+++ src/transformer.py
@@ -1,20 +1,16 @@
 import copy
-import math
-import torch
 import torch.nn as nn
 
 
 class Transformer(nn.Module):
-    def __init__(self, model_dimension=512, num_of_layers=6, num_of_attn_heads=8, ffn_dimension=2048, dropout=0.1):
+    def __init__(self, model_dim=512, num_of_layers=6, attn_heads=8, ffn_dim=2048, dropout=0.1):
         super().__init__()
 
-        encoder_layer = EncoderLayer(model_dimension, num_of_attn_heads, ffn_dimension, dropout)
-        decoder_layer = DecoderLayer(model_dimension, num_of_attn_heads, ffn_dimension, dropout)
+        encoder_layer = EncoderLayer(model_dim, attn_heads, ffn_dim, dropout)
+        decoder_layer = DecoderLayer(model_dim, attn_heads, ffn_dim, dropout)
 
         self.encoder = Encoder(encoder_layer, num_of_layers)
         self.decoder = Decoder(decoder_layer, num_of_layers)
-
-        self.output_generator = OutputGenerator(model_dimension)
 
     def forward(self, src, src_mask, tgt, tgt_mask):
         src = self.encoder(src, src_mask)
@@ -40,16 +36,16 @@ class Encoder(nn.Module):
 
 
 class EncoderLayer(nn.Module):
-    def __init__(self, model_dimension, num_of_attn_heads, ffn_dimension, dropout):
+    def __init__(self, model_dim, attn_heads, ffn_dim, dropout):
         super().__init__()
 
-        self.mha = MultiHeadAttention(model_dimension, num_of_attn_heads, dropout)
+        self.mha = MultiHeadAttention(model_dim, attn_heads, dropout)
         self.mha_dropout = nn.Dropout(dropout)
-        self.mha_norm = nn.LayerNorm(model_dimension)
+        self.mha_norm = nn.LayerNorm(model_dim)
 
-        self.ffn = FeedForwardNet(model_dimension, ffn_dimension, dropout)
+        self.ffn = FeedForwardNet(model_dim, ffn_dim, dropout)
         self.ffn_dropout = nn.Dropout(dropout)
-        self.ffn_norm = nn.LayerNorm(model_dimension)
+        self.ffn_norm = nn.LayerNorm(model_dim)
 
     def forward(self, src, src_mask=None):
         # MultiHeadAttention sublayer
@@ -77,20 +73,20 @@ class Decoder(nn.Module):
 
 
 class DecoderLayer(nn.Module):
-    def __init__(self, model_dimension, num_of_attn_heads, ffn_dimension, dropout):
+    def __init__(self, model_dim, attn_heads, ffn_dim, dropout):
         super().__init__()
 
-        self.masked_mha = MultiHeadAttention(model_dimension, num_of_attn_heads, dropout)
+        self.masked_mha = MultiHeadAttention(model_dim, attn_heads, dropout)
         self.masked_mha_dropout = nn.Dropout(dropout)
-        self.masked_mha_norm = nn.LayerNorm(model_dimension)
+        self.masked_mha_norm = nn.LayerNorm(model_dim)
 
-        self.mha = MultiHeadAttention(model_dimension, num_of_attn_heads, dropout)
+        self.mha = MultiHeadAttention(model_dim, attn_heads, dropout)
         self.mha_dropout = nn.Dropout(dropout)
-        self.mha_norm = nn.LayerNorm(model_dimension)
+        self.mha_norm = nn.LayerNorm(model_dim)
 
-        self.ffn = FeedForwardNet(model_dimension, ffn_dimension, dropout)
+        self.ffn = FeedForwardNet(model_dim, ffn_dim, dropout)
         self.ffn_dropout = nn.Dropout(dropout)
-        self.ffn_norm = nn.LayerNorm(model_dimension)
+        self.ffn_norm = nn.LayerNorm(model_dim)
 
     def forward(self, src, tgt, src_mask, tgt_mask):
         # Masked MultiHeadAttention sublayer
@@ -117,53 +113,59 @@ class DecoderLayer(nn.Module):
 
 
 class MultiHeadAttention(nn.Module):
-    def __init__(self, model_dimension, num_of_attn_heads, dropout):
+    def __init__(self, model_dim, attn_heads, dropout=None):
         super().__init__()
 
-        assert model_dimension % num_of_attn_heads == 0, (
-            f'Model dimension {model_dimension} is expected to be a multiple '
-            f'of the number of attention heads {num_of_attn_heads}'
+        assert model_dim % attn_heads == 0, (
+            f'Model dimension {model_dim} is expected to be a multiple '
+            f'of the number of attention heads {attn_heads}'
         )
 
-        self.query_proj = nn.Linear(model_dimension, model_dimension)
-        self.key_proj = nn.Linear(model_dimension, model_dimension)
-        self.value_proj = nn.Linear(model_dimension, model_dimension)
-        self.output_proj = nn.Linear(model_dimension, model_dimension)
+        self.attn_heads = attn_heads
+        self.head_dim = model_dim // attn_heads
 
-        # self.dropout = nn.Dropout(dropout) # Not using atm. Wasn't in paper
+        self.query_proj = nn.Linear(model_dim, model_dim)
+        self.key_proj = nn.Linear(model_dim, model_dim)
+        self.value_proj = nn.Linear(model_dim, model_dim)
+        self.output_proj = nn.Linear(model_dim, model_dim)
+
+        self.dropout = nn.Dropout(dropout)  # Wasn't in original paper
 
     def attention(self, query, key, value, mask):
+
         return
 
     def forward(self, query, key, value, mask):
+        batch_size = query.shape[0]
 
+        # Project src/tgt query, key and value into model_dim/attn_heads subspaces.
+        # For computation efficiency they are all part of the same matrices
+        query = self.query_proj(query)
+        key = self.key_proj(key)
+        value = self.value_proj(value)
+
+        query = query.reshape(batch_size, -1, self.attn_heads, self.head_dim).transpose(1, 2)
+        key = key.reshape(batch_size, -1, self.attn_heads, self.head_dim).transpose(1, 2)
+        value = value.reshape(batch_size, -1, self.attn_heads, self.head_dim).transpose(1, 2)
+
+        attn_output = self.attention(query, key, value, mask)
 
         return
 
 
 class FeedForwardNet(nn.Module):
-    def __init__(self, model_dimension, ffn_dimension, dropout):
+    def __init__(self, model_dim, ffn_dim, dropout):
         super().__init__()
         self.ffn = nn.Sequential(
-            nn.Linear(model_dimension, ffn_dimension),
+            nn.Linear(model_dim, ffn_dim),
             nn.ReLU(),
             nn.Dropout(dropout),
-            nn.Linear(ffn_dimension, model_dimension)
+            nn.Linear(ffn_dim, model_dim)
         )
 
     def forward(self, input):
         output = self.ffn(input)
         return output
-
-
-class OutputGenerator(nn.Module):
-    def __init__(self, model_dimension, tgt_vocab_size):
-        super().__init__()
-        self.linear = nn.Linear(model_dimension, tgt_vocab_size)
-        self.softmax = nn.LogSoftmax(dim=-1)
-
-    def forward(self):
-        return
 
 
 def _get_copies(module, num_of_copies):
